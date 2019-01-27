5aec7b034f8392113d6c41a8ae2cbef0cb016cdb
Jacobo Tapia <jacob.et.cetera@gmail.com>
2019-01-27 15:45:16 -0600

    init

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("app"));
